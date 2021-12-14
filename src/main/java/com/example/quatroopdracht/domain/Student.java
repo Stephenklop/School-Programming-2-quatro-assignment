4e05b973877357c350020c7982e0036dcd08c3b7
Stephen Klop <stefklop18@gmail.com>
2021-12-14 16:46:14 +0100

    Fixed UI

--- src/main/java/com/example/quatroopdracht/domain/Student.java
+++ src/main/java/com/example/quatroopdracht/domain/Student.java
@@ -1,5 +1,7 @@
 package com.example.quatroopdracht.domain;
 
+import javafx.scene.control.Button;
+
 public class Student {
     private String email;
     private String name;
@@ -8,8 +10,49 @@ public class Student {
     private String address;
     private String residence;
     private String country;
+    private Button updateButton;
+    private Button deleteButton;
 
+    public Student(String email, String name, String gender, String dateOfBirth, String address, String residence, String country) {
+        this.email = email;
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.residence = residence;
+        this.country = country;
+        this.updateButton = new Button("Update");
+        this.deleteButton = new Button("Delete");
+    }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", address='" + address + '\'' +
+                ", residence='" + residence + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    public Button getUpdateButton() {
+        return updateButton;
+    }
+
+    public void setUpdateButton(Button updateButton) {
+        this.updateButton = updateButton;
+    }
+
+    public Button getDeleteButton() {
+        return deleteButton;
+    }
+
+    public void setDeleteButton(Button deleteButton) {
+        this.deleteButton = deleteButton;
+    }
 
     public String getEmail() {
         return email;
--- src/main/java/com/example/quatroopdracht/ui/student/GetStudent.java
+++ src/main/java/com/example/quatroopdracht/ui/student/GetStudent.java
@@ -1,4 +1,57 @@
 package com.example.quatroopdracht.ui.student;
 
+import com.example.quatroopdracht.domain.Student;
+import com.example.quatroopdracht.ui.Dashboard;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class GetStudent {
+    Student[] students = {
+            new Student("test@test.nl", "Stef", "Male", "18-07-2001", "teststraat", "thuis", "Nederland"),
+            new Student("test@test1.nl", "Stef1", "Male", "18-07-2002", "teststraat1", "thuis1", "Nederland"),
+            new Student("test@test2.nl", "Stef2", "Male", "18-07-2003", "teststraat2", "thuis2", "Nederland"),
+            new Student("test@test3.nl", "Stef3", "Male", "18-07-2004", "teststraat3", "thuis3", "Nederland"),
+            new Student("test@test4.nl", "Stef4", "Male", "18-07-2005", "teststraat4", "thuis4", "Nederland")
+    };
+    public Scene getGetStudentScene(Stage stage) {
+
+        // Create layout
+        TableView<Student> tableStudents = new TableView<>();
+        tableStudents.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        VBox.setVgrow(tableStudents, Priority.ALWAYS);
+
+        TableColumn<Student, String> colEmail = new TableColumn<>("Email");
+        TableColumn<Student, String> colName = new TableColumn<>("Name");
+        TableColumn colUpdate = new TableColumn("Update");
+        TableColumn colDelete = new TableColumn("Delete");
+
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colUpdate.setCellValueFactory(new PropertyValueFactory<>("updateButton"));
+        colDelete.setCellValueFactory(new PropertyValueFactory<>("deleteButton"));
+
+
+        tableStudents.getColumns().addAll(colEmail, colName, colUpdate, colDelete);
+
+        tableStudents.getItems().addAll(students);
+
+        VBox vBox = new VBox(tableStudents);
+        vBox.setPadding(new Insets(10));
+        vBox.setSpacing(10);
+
+        Scene scene = new Scene(vBox);
+
+        return scene;
+    }
 }
