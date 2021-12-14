df258ca47bb4e60b0713aa3517361425690eedc1
FSGabrsek <f.gabrsek@gmail.com>
2021-12-14 21:56:16 +0100

    added update student

--- src/main/java/com/example/quatroopdracht/ui/student/UpdateStudent.java
+++ src/main/java/com/example/quatroopdracht/ui/student/UpdateStudent.java
@@ -1,5 +1,8 @@
 package com.example.quatroopdracht.ui.student;
 
+import com.example.quatroopdracht.data.StudentRepository;
+import com.example.quatroopdracht.domain.Student;
+import com.example.quatroopdracht.ui.Dashboard;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -11,7 +14,17 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.sql.Date;
+
 public class UpdateStudent {
+    private Student student;
+    private StudentRepository studentRepository;
+
+    public UpdateStudent(Student student) {
+        this.student = student;
+        this.studentRepository = new StudentRepository();
+    }
+
     public Scene getUpdateStudentScene(Stage stage) {
         // Create layout
         VBox vbox = new VBox();
@@ -19,7 +32,6 @@ public class UpdateStudent {
         GridPane gp = new GridPane();
 
         // Create labels
-        Label emailLabel = new Label("Email:");
         Label nameLabel = new Label("Name:");
         Label genderLabel = new Label("Gender:");
         Label dateOfBirthLabel = new Label("Date of birth:");
@@ -28,7 +40,6 @@ public class UpdateStudent {
         Label countryLabel = new Label("Country:");
 
         // Create input fields
-        TextField email = new TextField();
         TextField name = new TextField();
         ObservableList<String> genderList = FXCollections.observableArrayList("Male", "Female", "Other");
         ComboBox<String> gender = new ComboBox<>(genderList);
@@ -40,6 +51,23 @@ public class UpdateStudent {
         // Create buttons
         Button cancelButton = new Button("Cancel");
         Button submitButton = new Button("Submit");
+
+        submitButton.setOnAction(event -> {
+            student.setName(name.getText());
+            student.setGender(gender.getValue());
+            student.setDateOfBirth(new Date(dateOfBirth.getValue().getYear(), dateOfBirth.getValue().getMonth().getValue(), dateOfBirth.getValue().getDayOfMonth()));
+            student.setAddress(address.getText());
+            student.setResidence(residence.getText());
+            student.setCountry(country.getText());
+
+            studentRepository.updateStudent(student);
+
+            stage.setScene(new GetStudent().getGetStudentScene(stage));
+        });
+        cancelButton.setOnAction(event -> {
+            stage.setScene(new Dashboard().getDashboardScene(stage));
+        });
+
         buttonbox.getChildren().addAll(cancelButton, submitButton);
 
         // Set styling
@@ -48,14 +76,13 @@ public class UpdateStudent {
         gp.setVgap(8);
         VBox.setVgrow(gp, Priority.ALWAYS);
 
-        gp.add(emailLabel, 0, 0); gp.add(email, 1, 0);
-        gp.add(nameLabel, 0, 1); gp.add(name, 1, 1);
-        gp.add(genderLabel, 0, 2); gp.add(gender, 1, 2);
-        gp.add(dateOfBirthLabel, 0, 3); gp.add(dateOfBirth, 1,3);
-        gp.add(addressLabel, 0, 4); gp.add(address, 1, 4);
-        gp.add(residenceLabel, 0, 5); gp.add(residence, 1, 5);
-        gp.add(countryLabel, 0 ,6); gp.add(country, 1, 6);
-        gp.add(buttonbox, 1,7);
+        gp.add(nameLabel, 0, 0); gp.add(name, 1, 0);
+        gp.add(genderLabel, 0, 1); gp.add(gender, 1, 1);
+        gp.add(dateOfBirthLabel, 0, 2); gp.add(dateOfBirth, 1,2);
+        gp.add(addressLabel, 0, 3); gp.add(address, 1, 3);
+        gp.add(residenceLabel, 0, 4); gp.add(residence, 1, 4);
+        gp.add(countryLabel, 0 ,5); gp.add(country, 1, 5);
+        gp.add(buttonbox, 1,6);
 
         vbox.getChildren().addAll(gp);
 
