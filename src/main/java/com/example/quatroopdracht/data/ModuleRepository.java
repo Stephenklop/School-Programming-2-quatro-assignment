565e257394aabcca222136c1a464d6483b906128
Anika <anika.w.01@gmail.com>
2022-01-11 21:37:32 +0100

    ModuleRepository done, few todo's left for module class

--- src/main/java/com/example/quatroopdracht/data/ModuleRepository.java
+++ src/main/java/com/example/quatroopdracht/data/ModuleRepository.java
@@ -1,4 +1,164 @@
 package com.example.quatroopdracht.data;
 
+import com.example.quatroopdracht.domain.Module;
+import com.example.quatroopdracht.util.Util;
+import com.example.quatroopdracht.util.Validator;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class ModuleRepository extends DatabaseConnection{
+    public List<Module> getAllModules() {
+        String sql = "SELECT * FROM Module INNER JOIN Content ON Module.ContentID = Content.ContentID";
+        List<Module> modules = new ArrayList<>();
+
+        this.select(sql, resultSet -> {
+            try {
+                while (resultSet.next()) {
+                    modules.add(new Module(
+                            resultSet.getInt("ContentID"),
+                            resultSet.getDate("PublicationDate"),
+                            resultSet.getString("Status"),
+                            resultSet.getString("Title"),
+                            resultSet.getInt("Version"),
+                            resultSet.getString("Description"),
+                            resultSet.getString("CourseName"), //TODO in class module, there is a course, not a courseName
+                            resultSet.getString("EmailContactperson"), //TODO in class module, there is a contactperson, not an emailContactperson
+                            resultSet.getString("SerialNumberCourse"),
+                            resultSet.getInt("SerialNumberCourse")
+                    ));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        return modules;
+    }
+
+    public Module getModule(int ContentID) {
+        AtomicReference<Module> module = new AtomicReference<>(null);
+        String sql = String.format(
+                "SELECT * FROM Module WHERE ContentID = ‘%s’ INNER JOIN Content ON Module.ContentID = Content.ContentID",
+                ContentID
+        );
+
+        this.select(sql, resultSet -> {
+            try {
+                if (resultSet.isBeforeFirst() && resultSet.next()) {
+                    module.set(new Module(
+                            ContentID,
+                            resultSet.getDate("PublicationDate"),
+                            resultSet.getString("Status"),
+                            resultSet.getString("Title"),
+                            resultSet.getInt("Version"),
+                            resultSet.getString("Description"),
+                            resultSet.getString("CourseName"), //TODO in class module, there is a course, not a courseName
+                            resultSet.getString("EmailContactperson"), //TODO in class module, there is a contactperson, not an emailContactperson
+                            resultSet.getString("SerialNumberCourse"),
+                            resultSet.getInt("SerialNumberCourse")
+                    ));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return module.get();
+    }
+
+    public boolean addModule(Module module) {
+        try {
+            Validator.validateContent(module);
+        } catch (Exception ex) {
+            Util.displayError(ex.getMessage());
+            return false;
+        }
+
+        String sql = String.format(
+                "INSERT INTO Module VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') INNER JOIN Content ON Module.ContentID = Content.ContentID",
+                module.getContentItemId(),
+                module.getPublicationDate(),
+                module.getStatus(),
+                module.getDescription(),
+                module.getTitle(),
+                module.getVersion(),
+                module.getEmailContactperson(), //TODO zie todo hierboven
+                module.getSerialNumber(),
+                module.getCourseName()
+        );
+
+        boolean inserted = this.insert(sql);
+
+        if (inserted) {
+            Util.displaySuccess("Successfully created module!");
+            return true;
+        } else {
+            Util.displayError("An exception occurred!");
+            return false;
+        }
+    }
+
+    public boolean updateModule(Module module) {
+        try {
+            Validator.validateContent(module);
+        } catch (Exception ex) {
+            Util.displayError(ex.getMessage());
+            return false;
+        }
+
+        String sql = String.format(
+                "UPDATE Module SET SET PublicationDate = ‘%s’, Status = ‘%s’, Description = ‘%s’ " +
+                        "EmailContactperson = ‘%s’, SerialNumberCourse = ‘%s’, CourseName = ‘%s’ " +
+                        "WHERE  Title = ‘%s’ AND Version = ‘%s’ AND ContentID = ‘%s’" +
+                        "INNER JOIN Content ON Module.ContentID = Content.ContentID",
+                module.getPublicationDate(),
+                module.getStatus(),
+                module.getDescription(),
+                module.getEmailContactperson(), //TODO zie todo hierboven
+                module.getSerialNumber(),
+                module.getCourseName(),
+                module.getTitle(),
+                module.getVersion(),
+                module.getContentItemId()
+        );
+
+        int updated = this.update(sql);
+
+        switch (updated) {
+            case 0:
+                Util.displayError("Unable to find module!");
+                return false;
+            case -1:
+                Util.displayError("An exception occurred!");
+                return false;
+            default:
+                Util.displaySuccess("Successfully updated module!");
+                return true;
+        }
+    }
+
+    public boolean deleteModule(int ContentID, String Title, int Version) {
+        String sql = String.format(
+                "DELETE FROM Module WHERE Title = ‘%s’ AND Version = ‘%s’ AND ContentID = ‘%s’ INNER JOIN Content ON Module.ContentID = Content.ContentID",
+                Title,
+                Version,
+                ContentID
+        );
+
+        int deleted = this.update(sql);
+
+        switch (deleted) {
+            case 0:
+                Util.displayError("Unable to find module!");
+                return false;
+            case -1:
+                Util.displayError("An exception occurred!");
+                return false;
+            default:
+                Util.displaySuccess("Successfully deleted module!");
+                return true;
+        }
+    }
 }
